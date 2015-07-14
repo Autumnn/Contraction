a71a07703bf990cd173a25fe35f2c921f510d8af
Autumnn <apple@applemaoMacBook.fritz.box>
2015-07-14 15:36:25 +0200

    Second
    Second

--- Sample.java
+++ Sample.java
@@ -6,10 +6,10 @@ class Sample{
 
 	public void category(long[] Original_Data, long mean){
 
-	int Length = Data.length();
-	public long[] Data = Original_Data;
-	public long[] A = new long[Length];
-	public long[] B = new long[Length];
+	int Length = Data.length;
+	long[] Data = Original_Data;
+	long[] A = new long[Length];
+	long[] B = new long[Length];
 	Result = new long[Length][Length];
 
 
@@ -42,7 +42,38 @@ class Sample{
 	}
 
 	// The second run: two users pay for one.
-	  		
+	for(int i=0; i<Length; i++){
+		if(A[i] != 0) {
+			for(int j=0; j<Length; j++){
+				if((B[j] != 0)&&(A[i] > (-B[j]))){
+					long d = A[i] + B[j];
+					for(int k=0; k<Length; k++){
+						if(d == (-B[k])){
+							Result[i][j] = -B[j];
+							Result[i][k] = -B[k];
+							Result[j][i] = B[j];
+							Result[k][i] = B[k];
+							A[i] = 0;
+							B[j] = 0;
+							B[k] = 0;
+						}
+					}
+				}else if((B[j] != 0)&&(A[i] < (-B[j]))){
+					long d = A[i] + B[j];
+					for(int k=0; k<Length; k++){
+						if(d == (-A[k])){
+							Result[i][j] = A[i];
+							Result[k][j] = A[k];
+							Result[j][i] = -A[i];
+							Result[j][k] = -A[k];
+							A[i] = 0;
+							B[j] = 0;
+							A[k] = 0;
+						}
+					}
+			}
+		}
+	}		
 
 
 	}
